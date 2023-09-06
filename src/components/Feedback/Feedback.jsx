dd8666e961a3a2c2b6997cf5125d29ccda359d50
AnastasiiaPavlovska <nastiapavlovska446@gmail.com>
2023-09-06 12:55:02 +0200

    add another components

--- src/components/Feedback/Feedback.jsx
+++ src/components/Feedback/Feedback.jsx
@@ -1,2 +1,22 @@
 import React from "react";
 import PropTypes from 'prop-types';
+import {
+    FeedBack, FeedBackButton
+  } from  "./Feedback.styled"
+
+export default function Feedback ({options, onLeaveFeedback}) {
+    return (
+        <FeedBack>
+            {options.map(option => (
+                <FeedBackButton key={option} onClick={() => onLeaveFeedback(option)}>
+                    {option === 'good' ? 'Good😀' : option === 'neutral' ? 'Neutral😐' : 'Bad😡'}
+                </FeedBackButton>
+            ))}
+        </FeedBack>
+    )
+}
+
+Feedback.PropTypes = {
+    options: PropTypes.arrayOf(PropTypes.string).isRequired,
+    onLeaveFeedback: PropTypes.func.isRequired,
+};
